2cbfa345e131aa9ad88464771651149ffa519757
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-09-30 05:12:57 +0530

    feat(middleware) add class based and module based middleware in the nest js

--- app5_middleware/src/middlewares/logger.middleware.ts
+++ app5_middleware/src/middlewares/logger.middleware.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+// const LoggerMiddleware = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+// ) => {
+//   console.log('logger middleware ');
+//   next();
+// };
 
-const LoggerMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  console.log('logger middleware ');
-  next();
-};
 
-export default LoggerMiddleware;
+@Injectable()
+export class LoggerMiddleware  implements NestMiddleware{
+  use(req:Request,res:Response,next:NextFunction){
+    console.log("class based logger middleware")
+    next();
+  }
+}
+
+
+
